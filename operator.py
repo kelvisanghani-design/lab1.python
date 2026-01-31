a463502bb491301e6b391671e1c24602ccccc059
kelvisanghani-design <kelvisanghani@gmail.com>
2026-01-31 08:08:39 +0530

    Add files via upload

---
+++ operator.py
@@ -0,0 +1,79 @@
+"""Python 3.7.9 (tags/v3.7.9:13c94747c7, Aug 17 2020, 18:01:55) [MSC v.1900 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information."""
+>>> #arithmetic operators
+a=5
+b=10
+print(a+b)
+print(a-b)
+print(a/b)
+
+#assignment operator
+  x=10
+  print(x)
+#unary minus
+  a=5
+  print(-a)
+#relational operators
+  p=10
+  q=5
+  print(p>q)
+  print(p<q)
+
+#logical operators
+ x=True
+ y=False
+ print(x and y)
+ print(x or y)
+ print(not x)
+
+#boolean value
+ is_pass=true
+ print(is_pass)
+
+#bitwise operator
+ a=5
+ b=3
+ print(a/b)
+
+#membership operator
+ number=(1,2,3,4)
+ print(2 in number)
+ print(5 is not number)
+
+#identify operators
+ x=10
+ y=10
+ print(x is y)
+ print(x is not y)
+
+
+ #output
+ ============================= RESTART: D:\kelviiii\yashvi.py ============================
+15
+-5
+0.5
+>>> 
+============================= RESTART: D:\kelviiii\yashvi.py ============================
+10
+>>> 
+============================= RESTART: D:\kelviiii\yashvi.py ============================
+-5
+>>> 
+============================= RESTART: D:\kelviiii\yashvi.py ============================
+True
+False
+>>>
+============================= RESTART: D:\kelviiii\yashvi.py ============================
+True
+>>> 
+============================= RESTART: D:\kelviiii\yashvi.py ============================
+1.6666666666666667
+>>> 
+============================= RESTART: D:\kelviiii\yashvi.py ============================
+True
+True
+>>> 
+============================= RESTART: D:\kelviiii\yashvi.py ============================
+True
+False
+>>> 
